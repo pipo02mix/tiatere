20b54dc2ba3b39981799f2875a5a3de6782ce3ae
Fernando Ripoll <fernando@cobrowser.net>
2017-08-15 11:39:09 +0200

    Fix typo on webhook

--- src/Infrastructure/index.php
+++ src/Infrastructure/index.php
@@ -30,7 +30,7 @@ $app->get('/about-me', function (Request $request) use ($app) {
 
 $app->post('/wh', function (Request $request) use ($app) {
     if (verifyRequest($request)) {
-        shell_exec( 'cd '__DIR__.'/../../ && git reset --hard HEAD && git pull' );
+        shell_exec( 'cd '.__DIR__.'/../../ && git reset --hard HEAD && git pull');
         shell_exec('/etc/init.d/nginx reload');
         return $app->json([], 201);
     }
